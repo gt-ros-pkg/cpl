427bc1337dfdb50ccf2ab03760cc4e0143e04e1d
Tucker Hermans <thermans@cc.gatech.edu>
2013-02-19 18:15:44 -0500

    Implemented and verified gaussian processes mean prediction for use in RBF network controller.

--- tabletop_pushing/src/tabletop_pushing/rbf_control.py
+++ tabletop_pushing/src/tabletop_pushing/rbf_control.py
@@ -4,6 +4,23 @@ class RBFControl:
     '''
     Class to implement an RBF network controller based on PILCO learning for use in control of the robot.
     '''
+    def feedbackControl(self, X):
+        inp = np.zeros((self.N,self.D))
+        for r in xrange(self.N):
+            inp[r,:] = self.Xpi.T[r,:]-X.T
+        U = np.zeros((self.E,1))
+        for i in xrange(self.E):
+            Lambda = self.lambdas[i]
+            sf2 = self.sf2s[i]
+            sigmaBeta2 = self.sigmaBeta2s[i]
+            t = np.matrix(inp)*Lambda
+            # Note not diving since we aren't using variance here
+            # B = Lambda*V*Lambda+np.eye(self.D)
+            # t = np.linalg.solve(B.T,t.T)
+            l = np.exp(-np.sum(np.multiply(t,t),1)/2)
+            lb = np.reshape(np.multiply(l.squeeze(),self.beta[:,i]),(self.N,1))
+            U[i] = sf2*np.sum(lb)
+        return U
 
     def loadRBFController(self, controller_path):
         '''
@@ -32,6 +49,9 @@ class RBFControl:
         Compute the RBF network weights in GP form
         '''
         self.beta = np.zeros((self.N, self.E))
+        self.lambdas = []
+        self.sf2s = []
+        self.sigmaBeta2s = []
         for i in xrange(self.E):
             Lambda = np.diag(np.exp(-1*self.Hyp[i,:self.D])) # Length scale
             sf2 = np.exp(2*self.Hyp[i,self.D]) # signal variance
@@ -46,6 +66,9 @@ class RBFControl:
             A = KX+sigmaBeta2*np.eye(self.N)
             B = self.Ypi[:,i]
             betai = np.linalg.solve(A, B)
+            self.lambdas.append(Lambda)
+            self.sf2s.append(sf2)
+            self.sigmaBeta2s.append(sigmaBeta2)
             self.beta[:,i] = betai.squeeze()
 
     def squaredExponentialKernelARD(self, x, c, ell, sf2):
