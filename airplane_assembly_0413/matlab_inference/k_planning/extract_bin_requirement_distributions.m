532b5b7382a4b5fcf49e4eb8bad0d9434c225d7e
nam <nam@xlap.(none)>
2013-06-02 13:09:09 -0400

    fix extract_bin_requirement_distributions.m

--- airplane_assembly_0413/matlab_inference/k_planning/extract_bin_requirement_distributions.m
+++ airplane_assembly_0413/matlab_inference/k_planning/extract_bin_requirement_distributions.m
@@ -19,17 +19,17 @@ for i=1:length(bin_id)
         
         if strcmp(g.name, symbol_names1{i})
             if strcmp('start', symbol_types1{i})
-                bin_distributions(i).bin_needed = g.start_distribution;
+                bin_distributions(i).bin_needed = g.i_final.start_distribution;
             else
-                bin_distributions(i).bin_needed = g.end_distribution;
+                bin_distributions(i).bin_needed = g.i_final.end_distribution;
             end
         end
         
         if strcmp(g.name, symbol_names2{i})
             if strcmp('start', symbol_types2{i})
-                bin_distributions(i).bin_nolonger_needed = g.start_distribution;
+                bin_distributions(i).bin_nolonger_needed = g.i_final.start_distribution;
             else
-                bin_distributions(i).bin_nolonger_needed = g.end_distribution;
+                bin_distributions(i).bin_nolonger_needed = g.i_final.end_distribution;
             end
             
         end
