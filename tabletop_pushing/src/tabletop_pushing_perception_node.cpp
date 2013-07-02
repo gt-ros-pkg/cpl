aeb6b6ab689024d66d44964814903a94ddf6bf6d
Tucker Hermans <thermans@cc.gatech.edu>
2013-07-02 16:38:01 -0400

    Switch to use MPS for getting table plane for service callback.

--- tabletop_pushing/src/tabletop_pushing_perception_node.cpp
+++ tabletop_pushing/src/tabletop_pushing_perception_node.cpp
@@ -1882,7 +1882,7 @@ class TabletopPushingPerceptionNode
     XYZPointCloud obj_cloud, table_cloud;
     // TODO: Comptue the hull on the first call
     Eigen::Vector4f table_centroid;
-    pcl_segmenter_->getTablePlane(cloud, obj_cloud, table_cloud, table_centroid);
+    pcl_segmenter_->getTablePlaneMPS(cloud, obj_cloud, table_cloud, table_centroid);
     PoseStamped p;
     p.pose.position.x = table_centroid[0];
     p.pose.position.y = table_centroid[1];
