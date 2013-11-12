0ee15d4e62328a925a24c24006bac6967e70a103
Tucker Hermans <thermans@cc.gatech.edu>
2013-11-12 13:31:46 -0500

    Setup prediction function for SVM dynamics model.

--- tabletop_pushing/src/tabletop_pushing/model_based_pushing_control.py
+++ tabletop_pushing/src/tabletop_pushing/model_based_pushing_control.py
@@ -36,7 +36,7 @@ import rospy
 from geometry_msgs.msg import PoseStamped, TwistStamped, Twist
 from tabletop_pushing.srv import *
 from tabletop_pushing.msg import *
-from math import copysign
+from math import copysign, pi
 import svmutil
 
 class ModelPredictiveController:
@@ -58,19 +58,18 @@ class NaiveInputModel:
         return next_state
 
 class SVMPushModel:
-    def __init__(self, svm_files=None, epsilons=None, kernel_type=None, m=3):
+    def __init__(self, svm_file_names=None, epsilons=None, kernel_type=None, m=3):
         '''
         svm_files - list of svm model files to read from disk
         epsilons - list of epislon values for the epislon insensitive loss function (training only)
         kernel_type - type of kernel to use for traning, can be any of the self.KERNEL_TYPES keys (training only)
         m - number of output dimensions in the model (training only)
         '''
-        if svm_files is not None:
-            for svm_file in svm_files:
-                self.svm_models.append(svm_load_model(svm_file))
-            m = len(svm_files)
-        else:
-            self.svm_models = []
+        self.svm_models = []
+        if svm_file_names is not None:
+            for svm_file_name in svm_file_names:
+                # print 'Loading file', svm_file_name
+                self.svm_models.append(svmutil.svm_load_model(svm_file_name))
 
         if epsilons is not None:
             self.epsilons = epsilons
@@ -87,19 +86,46 @@ class SVMPushModel:
 
 
     def predict(self, cur_state, ee_pose, u):
-        # TODO: get feats
-        y = 0.0
-        x = self.transform_state_data_to_feat_vector(cur_state, ee_pose, u)
-        # TODO: perform prediction
-        svmutil.svm_predict(y, x, self.svm_model)
-        # TODO: transform prediction to correct state / class
+        '''
+        Predict the next state given current state estimates and control input
+        cur_sate - current state estimate of form VisFeedbackPushTrackingFeedback()
+        ee_pose - current end effector pose estimate of type PoseStamped()
+        u - control to evaluate of type TwistStamped()
+        '''
+        # Get feature vector
+        x = [self.transform_state_data_to_feat_vector(cur_state, ee_pose, u)]
+        # Perform predictions
+        Y_hat = []
+        for svm_model in self.svm_models:
+            y = [0]
+            [y_hat, _, _] = svmutil.svm_predict(y, x, svm_model)
+            Y_hat.append(y_hat[0])
+        # print 'Y_hat = ', Y_hat
+        # Transform predictions to correct state / class
         next_state = VisFeedbackPushTrackingFeedback()
+        next_state.x.x = cur_state.x.x + Y_hat[0]
+        next_state.x.y = cur_state.x.y + Y_hat[1]
+        next_state.x.theta = cur_state.x.theta + Y_hat[2]
         return next_state
 
     def transform_state_data_to_feat_vector(self, cur_state, ee_pose, u):
-        pass
+        '''
+        Get SVM feature vector from current state information.
+        Needs to be updated whenever transform_trial-data_to_feat_vectors() is updated
+        cur_sate - current state estimate of form VisFeedbackPushTrackingFeedback()
+        ee_pose - current end effector pose estimate of type PoseStamped()
+        u - control to evaluate of type TwistStamped()
+        '''
+        return [cur_state.x.x, cur_state.x.y, cur_state.x.theta,
+                ee_pose.pose.position.x, ee_pose.pose.position.y,
+                u.twist.linear.x, u.twist.linear.y]
 
     def transform_trial_data_to_feat_vectors(self, trajectory):
+        '''
+        Get SVM feature vector from push trial trajectory state information.
+        Needs to be updated whenever transform_state_data_to_feat_vector() is updated
+        trajectory - list of ControlTimeStep() defined in push_learning.py
+        '''
         X = []
         Y = []
         for i in xrange(len(trajectory)-1):
@@ -114,6 +140,10 @@ class SVMPushModel:
         return (X, Y)
 
     def learn_model(self, learn_data):
+        '''
+        Learns SVMs to predict dynamics given the input data
+        learn_data - List of PushCtrlTrial() defined in push_learning.py
+        '''
         X = []
         Y = []
         # print 'len(learn_data)',len(learn_data)
@@ -132,16 +162,46 @@ class SVMPushModel:
             svm_model = svmutil.svm_train(Y_i, X, param_string)
             self.svm_models.append(svm_model)
 
-    def save_model(self, output_paths):
+    def save_models(self, output_file_names):
+        '''
+        Write svm models to disk
+        output_file_names - list of file paths for saving to, assumes length = len(self.svm_models)
+        '''
         if len(self.svm_models) > 0:
-            for path, model in zip(output_paths, self.svm_models):
-                svmutil.svm_save_model(path, model)
+            for file_name, model in zip(output_file_names, self.svm_models):
+                svmutil.svm_save_model(file_name, model)
 
-# import sys
-# import push_learning
-# if __name__ == '__main__':
-#     aff_file_name  = sys.argv[1]
-#     plio = push_learning.CombinedPushLearnControlIO()
-#     plio.read_in_data_file(aff_file_name)
-#     svm_dynamics = SVMPushModel()
-#     svm_dynamics.learn_model(plio.push_trials)
+#
+# Offline Testing
+#
+import sys
+import push_learning
+if __name__ == '__main__':
+    aff_file_name  = sys.argv[1]
+    plio = push_learning.CombinedPushLearnControlIO()
+    plio.read_in_data_file(aff_file_name)
+    svm_dynamics = SVMPushModel()
+    svm_dynamics.learn_model(plio.push_trials)
+    base_path = '/u/thermans/data/svm_dyn/'
+    output_paths = []
+    output_paths.append(base_path+'delta_x_dyn.model')
+    output_paths.append(base_path+'delta_y_dyn.model')
+    output_paths.append(base_path+'delta_theta_dyn.model')
+    svm_dynamics.save_models(output_paths)
+    svm_dynamics2 = SVMPushModel(svm_file_names=output_paths)
+
+    test_pose = VisFeedbackPushTrackingFeedback()
+    test_pose.x.x = 0.2
+    test_pose.x.y = 0.0
+    test_pose.x.theta = pi*0.5
+    test_ee = PoseStamped()
+    test_ee.pose.position.x = test_pose.x.x - 0.2
+    test_ee.pose.position.y = test_pose.x.y - 0.2
+    test_u = TwistStamped()
+    test_u.twist.linear.x = 0.3
+    test_u.twist.linear.y = 0.3
+
+    next_state = svm_dynamics2.predict(test_pose, test_ee, test_u)
+
+    print 'test_state.x: ', test_pose.x
+    print 'next_state.x: ', next_state.x
