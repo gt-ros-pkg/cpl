Fixed error in summing up object function and gradient values.

tabletop_pushing/src/tabletop_pushing/model_based_pushing_control.py
@@ -67,8 +67,11 @@ def pushMPCObjectiveFunction(q, H, n, m, x0, x_d, xtra, dyn_model):
         x_stop = x_start+x_d_length
         # Pull out x from q
         x_k_plus_1 = np.asarray(q[x_start:x_stop])
+        # print 'x_k',x_k_plus_1
+        # print 'x_d',x_d[k+1]
         # Compute sum of squared error on current trajectory time step
-        cost = sum((x_k_plus_1 - x_d[k+1])**2)
+        cost += sum((x_k_plus_1 - x_d[k+1])**2)
+        # print 'cost[',k,'] =',cost
     return cost
 
 def pushMPCObjectiveGradient(q, H, n, m, x0, x_d, xtra, dyn_model):
@@ -79,7 +82,7 @@ def pushMPCObjectiveGradient(q, H, n, m, x0, x_d, xtra, dyn_model):
     for k in xrange(H):
         x_i_start = m+k*step
         for j, i in enumerate(range(x_i_start, x_i_start+x_d_length)):
-            gradient[i] = 2.0*(q[i]-x_d[k+1][j])
+            gradient[i] += 2.0*(q[i]-x_d[k+1][j])
     return gradient
 
 def pushMPCConstraints(q, H, n, m, x0, x_d, xtra, dyn_model):